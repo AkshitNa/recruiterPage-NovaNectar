e9b460f094ba84ba56160134c11d2afe467bc472
Akshit Nautiyal <nauti.akshit001@gmail.com>
2025-01-18 10:37:22 +0530

    Add MAP 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,130 +1,166 @@
 //For Routing
-import { Outlet, RouterProvider, Navigate, createBrowserRouter, ScrollRestoration } from "react-router";
+import {
+  Outlet,
+  RouterProvider,
+  Navigate,
+  createBrowserRouter,
+  ScrollRestoration,
+} from "react-router";
 
 //Layout
-import { Footer, Header, Error, ReferEarn } from "./Components/Layout/IndexLayout";
+import {
+  Footer,
+  Header,
+  Error,
+  ReferEarn,
+} from "./Components/Layout/IndexLayout";
 
 //Pages
-import { About, Blog, Contact, FAQ, HomeImages, Process, Testimonials } from "./Components/Home/ZHomeIndex";
+import {
+  About,
+  Blog,
+  Contact,
+  FAQ,
+  HomeImages,
+  Process,
+  Testimonials,
+} from "./Components/Home/ZHomeIndex";
 
-//Apply Form
-import ApplyForm from "./Components/Layout/ApplyForm";
+//Map
+
+import NovaNectarMap from "./Components/Home/NovaNectarMap";
 
 //Toastify
-import { ToastContainer, } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 
 //AppLayout
 const AppLayout = () => {
-
   return (
     <div>
-      <ScrollRestoration/> {/* To get to top */}
-      <Header/>
-      < ToastContainer
-      position="top-center"
-      draggable={true}
-      toastStyle={{ marginTop: "100px" }}
-      /> 
+      <ScrollRestoration /> {/* To get to top */}
+      <Header />
+      <ToastContainer
+        position="top-center"
+        draggable={true}
+        toastStyle={{ marginTop: "100px" }}
+      />
       <main>
-      <Outlet/> {/* Populate our other pages and components */}
+        <Outlet /> {/* Populate our other pages and components */}
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 };
 
 const router = createBrowserRouter([
-
   {
     path: "/",
-    element: <AppLayout/>,
+    element: <AppLayout />,
     children: [
       {
         path: "/",
-        element: <> <HomeImages/> 
-        <About/>
-        <Process/>
-        <Blog/>
-        <Testimonials/>
-        <Contact/>
-        <FAQ/>
-        </>
-      }
-    ]
+        element: (
+          <>
+            {" "}
+            <HomeImages />
+            <About />
+            <Process />
+            <Blog />
+            <Testimonials />
+            <Contact />
+            <FAQ />
+          </>
+        ),
+      },
+    ],
   },
   {
     path: "/process",
-    element: <AppLayout/>,
+    element: <AppLayout />,
     children: [
       {
         path: "/process",
-        element: <>
-        <Process/>
-        <Blog/>
-        <Testimonials/>
-        </>
-      }
-    ]
+        element: (
+          <>
+            <Process />
+            <Blog />
+            <Testimonials />
+          </>
+        ),
+      },
+    ],
   },
   {
     path: "/blog",
-    element: <AppLayout/>,
+    element: <AppLayout />,
     children: [
       {
         path: "/blog",
-        element: <>
-        <Blog/>
-        <Testimonials/>
-        <Contact/>
-        </>
-      }
-    ]
+        element: (
+          <>
+            <Blog />
+            <Testimonials />
+            <Contact />
+          </>
+        ),
+      },
+    ],
   },
   {
     path: "/referAndEarn",
-    element: <AppLayout/>,
+    element: <AppLayout />,
     children: [
       {
         path: "/referAndEarn",
-        element: <>
-        <ReferEarn/>
-        <Blog/>
-        <Testimonials/>
-        </>
-      }
-    ]
+        element: (
+          <>
+            <ReferEarn />
+            <Blog />
+            <Testimonials />
+          </>
+        ),
+      },
+    ],
   },
   {
     path: "/contact",
-    element: <AppLayout/>,
+    element: <AppLayout />,
     children: [
       {
         path: "/contact",
-        element: <> <Contact/>
-        </>
-      }
-    ]
+        element: (
+          <>
+            {" "}
+            <Contact />
+            <NovaNectarMap/>
+          </>
+        ),
+      },
+    ],
   },
   {
     path: "/apply",
-    element: <AppLayout/>,
+    element: <AppLayout />,
     children: [
       {
         path: "/apply",
-        element: <>  <Navigate to = "/"/> 
-        </>
-      }
-    ]
+        element: (
+          <>
+            {" "}
+            <Navigate to="/" />
+          </>
+        ),
+      },
+    ],
   },
   {
     path: "*",
-    element: <Error/>
-  }
-  
+    element: <Error />,
+  },
 ]);
 
 function App() {
-  
-  return <RouterProvider router={router}/>;}
+  return <RouterProvider router={router} />;
+}
 
 export default App;
--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -111,13 +111,6 @@ export const Contact = () => {
           </form>
         </div>
 
-        <div className="w-full aspect-video md:w-[600px] lg:w-[800px] mt-5">
-  <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3444.507785521921!2d78.0035169750208!3d30.308071206032984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39092be94375e729%3A0xc160311fe8cb82d6!2sNovaNectar%20Services%20Pvt.%20Ltd.!5e0!3m2!1sen!2sus!4v1737175269424!5m2!1sen!2sus"
-    className="w-full h-full border-0"
-  ></iframe>
-</div>
-
       </div>
     </div>
   );
